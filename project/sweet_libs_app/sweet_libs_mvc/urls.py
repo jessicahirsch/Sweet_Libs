fc0c96cbc269c7421c62d25b439c89a736a55e0d
Jessica Hirsch <jrachelh5@gmail.com>
2017-05-06 16:42:24 -0400

    all pages linked and static set up

--- project/sweet_libs_app/sweet_libs_mvc/urls.py
+++ project/sweet_libs_app/sweet_libs_mvc/urls.py
@@ -4,4 +4,5 @@ from sweet_libs_mvc import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^results/$', views.ResultsPageView.as_view()),
+    url(r'^polls/$', views.PollPageView.as_view()),
 ]
