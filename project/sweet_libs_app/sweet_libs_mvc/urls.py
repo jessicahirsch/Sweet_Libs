99540a9eb79230f12557bed98cd597e723da6bd4
Artur Lan <arturlan.us@gmail.com>
2017-05-06 15:24:59 -0400

    route to results

--- project/sweet_libs_app/sweet_libs_mvc/urls.py
+++ project/sweet_libs_app/sweet_libs_mvc/urls.py
@@ -4,4 +4,5 @@ from sweet_libs_mvc import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^about/$', views.AboutPageView.as_view()),
+    url(r'^results/$', views.ResultsPageView.as_view()),
 ]
