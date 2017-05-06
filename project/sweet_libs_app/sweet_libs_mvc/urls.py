e06c7af56799736fd1007d0f783dd30105941058
Khem Marriott <KhemMarriott@Gmail.com>
2017-05-06 16:52:29 -0400

    orking views.py

--- project/sweet_libs_app/sweet_libs_mvc/urls.py
+++ project/sweet_libs_app/sweet_libs_mvc/urls.py
@@ -4,6 +4,5 @@ from sweet_libs_mvc import views
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
     url(r'^results/$', views.ResultsPageView.as_view()),
-    url(r'^poll/$', views.AboutPageView.as_view()),
-
+    url(r'^poll/$', views.PollPageView.as_view()),
 ]
